8d1c3efeaba6912b525433a4951ec3bf7bb64cc5
Jed Dev <getesjohnjed09@gmail.com>
2026-01-21 10:54:39 +0800

    Fixed variable undefined by adding access.

--- src/main.js
+++ src/main.js
@@ -32,7 +32,7 @@ for (var i = 0; i < imgLinks.childElementCount; i++) {
 
 //copy longdesc attribute from img to clipboard
 function copyLinkFromImg() {
-  imgDesc = this.getAttribute("longdesc");
+  var imgDesc = this.getAttribute("longdesc");
 
   // Create a dummy input to copy the string array inside it
   var dummy = document.createElement("input");
