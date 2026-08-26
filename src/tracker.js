d0460b7ea06f55bfb044a505914f1e907da8e439
Jed Dev <getesjohnjed09@gmail.com>
2026-08-26 22:25:42 +0800

    Dynamically uses correct backend url either local or production

--- src/tracker.js
+++ src/tracker.js
@@ -1,6 +1,11 @@
-const API_URL = "http://127.0.0.1:8000/api/v1/track";
+const isLocal = ["localhost", "127.0.0.1"].includes(window.location.hostname);
+const API_URL = isLocal
+  ? "http://127.0.0.1:8000"
+  : "https://pulse-trace.onrender.com";
 
 export function initTracker() {
+  const endpoint = `${API_URL}/api/v1/track`;
+
   function sendTrackingData() {
     const payload = {
       path: window.location.pathname + window.location.search,
@@ -12,9 +17,9 @@ export function initTracker() {
       const blob = new Blob([JSON.stringify(payload)], {
         type: "application/json",
       });
-      navigator.sendBeacon(API_URL, blob);
+      navigator.sendBeacon(endpoint, blob);
     } else {
-      fetch(API_URL, {
+      fetch(endpoint, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(payload),
